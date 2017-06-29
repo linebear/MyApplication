eeb1d29917b036b07d57dc9c8f1fffed9285867c
ThomasHsu <thomas_hsu@clientron.com>
2017-06-29 17:03:37 +0800

    Test3

--- app/src/main/java/com/clientron/a03624/myapplication/MainActivity.java
+++ app/src/main/java/com/clientron/a03624/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     private void init(){
-        
+        System.out.println("hi");
+
     }
 }
